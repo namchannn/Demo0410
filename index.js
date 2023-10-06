6705f76ba98227e0e6e7a80936787405222e6dd1
Nam Chan <namanhseoul@gmail.com>
2023-10-06 15:54:41 +0700

    update

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express(); // tạo ra localhost
 const PORT = 3333;
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.listen(PORT, function() {
     console.log("Server is running...");
 });
 
 // Routes
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/views/home.html");
+    res.sendFile(__dirname + "/public/views/login.html");
+});
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/views/register.html");
 });
 app.get("/about-us", function(req, res) {
     res.send("About us page");
