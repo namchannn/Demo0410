b4dc357fa02601f577f0b4a0de79c75bc95e4736
Nam Chan <namanhseoul@gmail.com>
2023-10-06 21:08:10 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express(); // tạo ra localhost
 const PORT = 3333;
 
-app.use("/public", express.static(__dirname + "/public"));
+// connect mysql
+const configDB = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 't2305e',
+    multipleStatements: true // cho phép viết nhiều câu SQL trong 1 lần query
+}
+const mysql = require("mysql");
+const conn = mysql.createConnection(configDB);
+
+// cấp phép cho sử dụng các file static
+app.use(express.static("public"));
 
 app.listen(PORT, function() {
     console.log("Server is running...");
@@ -17,4 +30,49 @@ app.get("/", function(req, res) {
 });
 app.get("/about-us", function(req, res) {
     res.send("About us page");
+});
+
+app.get("/api/categories", function(req, res) {
+    const sql = `select * from categories`;
+    conn.query(sql, function(err, rs) {
+        if (err) return res.send("Error");
+        return res.send(rs);
+    });
+});
+app.get("/api/products", function(req, res) {
+    const sql = `select * from products`;
+    conn.query(sql, function(err, rs) {
+        if (err) return res.send("Error");
+        return res.send(rs);
+    });
+});
+// api hiển thị các product theo tên
+app.get("/api/search", function(req, res) {
+    // get paramaters
+    const search = req.query.q;
+    const sql = `select * from products where name like '%${search}%'`;
+    conn.query(sql, function(err, rs) {
+        if (err) return res.send("Error");
+        return res.send(rs);
+    });
+});
+// api hiển thị các product trong categories tìm kiếm
+app.get("/api/category/:id", function(req, res) {
+    // get paramaters
+    const id = req.params.id;
+    const sql = `select * from products where category_id = ${id}`;
+    conn.query(sql, function(err, rs) {
+        if (err) return res.send("Error");
+        return res.send(rs);
+    });
+});
+// chi tiết 1 product
+app.get("/api/product/:name", function(req, res) {
+    // get paramaters
+    const name = req.params.name;
+    const sql = `select * from products where name like '%${name}%'`;
+    conn.query(sql, function(err, rs) {
+        if (err) return res.send("Error");
+        return res.send(rs);
+    });
 });
