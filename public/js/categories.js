47d4697aef6458bb77e1176b2f0f85030afacaca
hieuhisky123 <vuminhhieu11041997@gmail.com>
2023-11-08 18:06:48 +0700

    update

--- public/js/categories.js
+++ public/js/categories.js
@@ -11,69 +11,69 @@
 //     });
 // })();
 
-(() => {
-    document.addEventListener("DOMContentLoaded", () => {
-      // Xử lý khi một danh mục được chọn
-      const handleCategoryChange = (category) => {
-        // Xây dựng URL dựa trên danh mục
-        const url = category ? `http://localhost:3333/api/categories/${category}` : `http://localhost:3333/api/products`;
+// (() => {
+//     document.addEventListener("DOMContentLoaded", () => {
+//       // Xử lý khi một danh mục được chọn
+//       const handleCategoryChange = (category) => {
+//         // Xây dựng URL dựa trên danh mục
+//         const url = category ? `http://localhost:3333/api/categories/${category}` : `http://localhost:3333/api/products`;
         
         
-        // Gọi API và xử lý kết quả
-        axios.get(url)
-          .then(response => {
-            // Xóa danh sách sản phẩm hiện tại và hiển thị sản phẩm mới
-            displayProducts(response.data);
-          })
-          .catch(error => {
-            console.error('Có lỗi khi tải sản phẩm:', error);
-          });
-      };
+//         // Gọi API và xử lý kết quả
+//         axios.get(url)
+//           .then(response => {
+//             // Xóa danh sách sản phẩm hiện tại và hiển thị sản phẩm mới
+//             displayProducts(response.data);
+//           })
+//           .catch(error => {
+//             console.error('Có lỗi khi tải sản phẩm:', error);
+//           });
+//       };
   
-      // Lắng nghe sự kiện click cho tất cả liên kết danh mục
-      document.querySelectorAll('.sidebar_categories li a').forEach(link => {
-        link.addEventListener('click', function (e) {
-          e.preventDefault();
+//       // Lắng nghe sự kiện click cho tất cả liên kết danh mục
+//       document.querySelectorAll('.sidebar_categories li a').forEach(link => {
+//         link.addEventListener('click', function (e) {
+//           e.preventDefault();
           
-          // Xóa class 'active' và thêm vào mục được chọn
-          const categoryLinks = document.querySelectorAll('.sidebar_categories li');
-          categoryLinks.forEach(node => node.classList.remove('active'));
-          const parentLi = this.closest('li');
-          parentLi.classList.add('active');
+//           // Xóa class 'active' và thêm vào mục được chọn
+//           const categoryLinks = document.querySelectorAll('.sidebar_categories li');
+//           categoryLinks.forEach(node => node.classList.remove('active'));
+//           const parentLi = this.closest('li');
+//           parentLi.classList.add('active');
           
-          // Lấy danh mục được chọn và xử lý thay đổi
-          const category = this.dataset.category;
-          handleCategoryChange(category);
-        });
-      });
+//           // Lấy danh mục được chọn và xử lý thay đổi
+//           const category = this.dataset.category;
+//           handleCategoryChange(category);
+//         });
+//       });
       
-      // Khởi tạo danh sách sản phẩm với danh mục 'All'
-      handleCategoryChange("");
-    });
+//       // Khởi tạo danh sách sản phẩm với danh mục 'All'
+//       handleCategoryChange("");
+//     });
   
-    // Hàm hiển thị sản phẩm
-    function displayProducts(products) {
-      const productListEl = document.getElementById('product-list');
-      if (!productListEl) {
-        console.error('Không tìm thấy phần tử danh sách sản phẩm.');
-        return;
-      }
+//     // Hàm hiển thị sản phẩm
+//     function displayProducts(products) {
+//       const productListEl = document.getElementById('product-list');
+//       if (!productListEl) {
+//         console.error('Không tìm thấy phần tử danh sách sản phẩm.');
+//         return;
+//       }
       
-      // Xóa danh sách sản phẩm hiện tại
-      productListEl.innerHTML = '';
+//       // Xóa danh sách sản phẩm hiện tại
+//       productListEl.innerHTML = '';
       
-      // Lặp qua mỗi sản phẩm và thêm vào danh sách
-      products.forEach((product) => {
-        const productEl = document.createElement('div');
-        productEl.classList.add('product-item'); // Sửa lại class name cho phù hợp với CSS của bạn
+//       // Lặp qua mỗi sản phẩm và thêm vào danh sách
+//       products.forEach((product) => {
+//         const productEl = document.createElement('div');
+//         productEl.classList.add('product-item'); // Sửa lại class name cho phù hợp với CSS của bạn
         
 
         
-        productEl.innerHTML = `
-          <h3>${product.name}</h3>
-          <img src="${product.thumbnail}" alt="${product.name}">`;
-        productListEl.appendChild(productEl);
-      });
-    }
-  })();
+//         productEl.innerHTML = `
+//           <h3>${product.name}</h3>
+//           <img src="${product.thumbnail}" alt="${product.name}">`;
+//         productListEl.appendChild(productEl);
+//       });
+//     }
+//   })();
   
