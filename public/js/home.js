c95423dd827c69e281435f7f8326814c7459ae9e
hieuhisky123 <vuminhhieu11041997@gmail.com>
2023-11-13 16:44:05 +0700

    update

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,60 @@
 (() => {
-    document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("DOMContentLoaded", function () {
         axios.get('http://localhost:3333/api/products').then(function (response) {
             if (response.status === 200) {
                 const productListEl = document.getElementById('product-list');
                 const productData = response.data;
-                
+            
+   // Lấy các phần tử DOM
+const chatBot = document.getElementById("chatBot");
+const openChatButton = document.getElementById("openChat");
+const toggleChat = document.getElementById("toggleChat");
+const userInput = document.getElementById("userInput");
+const sendMessage = document.getElementById("sendMessage");
+
+// Định nghĩa các phản hồi tĩnh
+const responses = {
+  "Xin chào": "MADAMBOUTIQUE Chào bạn, tôi có thể giúp gì cho bạn?",
+  "xin chào": "MADAMBOUTIQUE Chào bạn, tôi có thể giúp gì cho bạn?",
+  "Chào": "MADAMBOUTIQUE Chào bạn, tôi có thể giúp gì cho bạn?",
+  "Chao": "MADAMBOUTIQUE Chào bạn, tôi có thể giúp gì cho bạn?",
+  "chào": "MADAMBOUTIQUE Chào bạn, tôi có thể giúp gì cho bạn?",
+  "hello": "MADAMBOUTIQUE Chào bạn, tôi có thể giúp gì cho bạn?",
+  "Giờ mở cửa": "Chúng tôi mở cửa từ 9 giờ sáng đến 5 giờ chiều.",
+  "gio mo cua": "Chúng tôi mở cửa từ 9 giờ sáng đến 5 giờ chiều.",
+  "giờ mở cửa": "Chúng tôi mở cửa từ 9 giờ sáng đến 5 giờ chiều."
+  // Thêm các câu hỏi và câu trả lời khác ở đây
+};
+
+// Hiển thị/ẩn chat bot khi nhấn nút X
+toggleChat.addEventListener("click", function () {
+  chatBot.classList.toggle("show-chat");
+});
+
+// Gửi tin nhắn và nhận phản hồi khi nhấn nút Gửi
+sendMessage.addEventListener("click", function () {
+  const userMessage = userInput.value;
+
+  // Xử lý tin nhắn của người dùng
+  const chatContent = chatBot.querySelector(".chat-content");
+  const userMessageElement = document.createElement("p");
+  userMessageElement.textContent = "Bạn: " + userMessage;
+  chatContent.appendChild(userMessageElement);
+
+  // Xử lý phản hồi từ chat bot
+  const response = responses[userMessage] || "Xin lỗi, tôi không hiểu câu hỏi của bạn.";
+  const botReplyElement = document.createElement("p");
+  botReplyElement.textContent = "Chat Bot: " + response;
+  chatContent.appendChild(botReplyElement);
+
+  userInput.value = ""; // Xóa nội dung nhập
+});
+
+// Hiển thị chat bot khi nhấn nút Chat
+openChatButton.addEventListener("click", function () {
+  chatBot.classList.add("show-chat");
+});
+
                 productData.forEach((prod) => {
                     const productEl = document.createElement("div");
                     productEl.classList = 'product-item col-3';
